020350475eb50dc48cfbbc9622b27123d21936ea
kuma-coffee <daniel099d@gmail.com>
2023-11-08 01:59:20 +0700

    minor fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from "vue";
+import { createApp } from "vue"; // Menggunakan "createApp" dari Vue 3
 import App from "./App.vue";
 import router from "./router/index";
+import axios from "axios";
+import VueCookies from "vue-cookies";
 
-createApp(App).use(router).mount("#app");
+var beAPI = axios.create({
+  baseURL: "http://localhost:8080",
+});
+
+export { beAPI };
+
+const app = createApp(App); // Membuat aplikasi Vue 3
+app.use(router);
+
+app.config.globalProperties.$be_http = beAPI; // Menggunakan "config.globalProperties" untuk menambahkan properti global
+app.mount("#app");
